4809fd65de2c3be603492dae385586197f524f7f
clem9281 <clem9281@gmail.com>
2019-04-03 15:39:13 -0700

    added restricted middleware and get route for the users

---
+++ users/user-routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../users/user-model");
+
+router.route("/").get(async (req, res) => {
+  try {
+    const users = await db.find();
+    const usersWithoutPasswords = users.map(user => ({
+      username: user.username,
+      department: user.department
+    }));
+    res.status(200).json(usersWithoutPasswords);
+  } catch (error) {
+    res.status(500).json({ message: "We could not get the users right now" });
+  }
+});
+
+module.exports = router;
