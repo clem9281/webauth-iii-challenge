4087a112efadc5a5347cb7181dd416b420c75a37
clem9281 <clem9281@gmail.com>
2019-04-04 14:18:04 -0700

    added logout button

--- user-client/src/components/NavigationBar.js
+++ user-client/src/components/NavigationBar.js
@@ -6,12 +6,16 @@ import {
   Collapse,
   NavItem,
   NavLink,
-  Nav
+  Nav,
+  Button
 } from "reactstrap";
 import { NavLink as RouterNavLink } from "react-router-dom";
 
 const NavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const logout = () => {
+    localStorage.removeItem("token");
+  };
   return (
     <Navbar color="light" light expand="md">
       <NavbarBrand href="/">WebAuth IV</NavbarBrand>
@@ -31,6 +35,11 @@ const NavigationBar = () => {
           <NavItem>
             <NavLink to="/users" tag={RouterNavLink}>
               Users
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/" tag={RouterNavLink} onClick={logout}>
+              Logout
             </NavLink>
           </NavItem>
         </Nav>
